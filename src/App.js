f0e3c6316901d80d078e647b380a5e2695e69e6a
sofialevin <slevin@M-4876.local>
2019-09-27 10:15:02 -0700

    Completed MVP.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 
-function App() {
+import Search from './components/Search';
+import RecipeList from './components/RecipesList';
+
+import { getRecipes } from "./actions";
+
+import { connect } from 'react-redux';
+
+function App(props) {
+  const [ searchInput, setSearchInput ] = useState("");
+
+  const textInput = React.createRef();
+
+  const handleChange = (event) => {
+    setSearchInput(event.target.value);
+}
+
+const addToSearch = (ingredient) => {
+    setSearchInput(searchInput + ingredient)
+}
+
+  const getRecipes = (ingredients) => {
+    props.getRecipes(ingredients.split(/[ ,]+/).join(','));
+    console.log(props);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getRecipes(searchInput);
+    setSearchInput("");
+}
+
   return (
     <div className="App">
+      <Search getRecipes={getRecipes} textInput={textInput} searchInput={searchInput} recipes={props.recipes} handleChange={handleChange} handleSubmit={handleSubmit}/>
+      <RecipeList recipes={props.recipes} textInput={textInput} addToSearch={addToSearch} isFetching={props.isFetching}/>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    recipes: state.recipes,
+    error: state.error,
+    isFetching: state.isFetching
+  };
+};
+
+export default connect(mapStateToProps, { getRecipes })(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import { reducer } from "./reducers";
+
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from "react-redux";
+
+import reducer from "./reducers";
+
+import thunk from 'redux-thunk';
+
+import './index.css';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
